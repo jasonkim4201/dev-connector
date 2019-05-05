ba86bb54a41103469d0ceb84c44ab1bc925b1ad7
Jason Kim <jasonkim4201@gmail.com>
2019-05-05 11:56:42 -0400

    added create and update operations in profiles

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -27,7 +27,7 @@ router.get("/", auth, async(req, res) => {
 // access value: public
 router.post("/", [
   check("email", "Please include a valid email").isEmail(),
-  check("password", "Please is required").exists()
+  check("password", "Password is required").exists()
 ],
   async (req, res) => {
 

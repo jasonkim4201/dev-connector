583c7ef98e2c6ef32c3bd72bc1a750a0217e8268
Jason Kim <jasonkim4201@gmail.com>
2019-05-07 12:03:33 -0400

    added post function on posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator/check");
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-// @route GET api/posts
-// description: test route
-// access value: public
+// @route POST api/posts
+// description: create a post
+// access value: private
 
-router.get("/", (req, res) => res.send("post route"));
+router.post("/", [auth, [
+    check("text", "Text is required").not().isEmpty()
+  ]
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array() });
+  } 
+
+  try {
+  const user = await User.findById(req.user.id).select("-password"); // to not bring pw back. because thats baaaaad
+  
+  const newPost = new Post ({
+    text: req.body.text,
+    name: user.name,
+    avatar: user.avatar,
+    user: req.user.id
+  });
+  
+  const post = await newPost.save();
+  res.json(post);
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+  
+});
 
 module.exports = router;

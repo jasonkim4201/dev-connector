e87056ae90d2ea60ab2c034dec5789a9d4c9ebaa
Jason Kim <jasonkim4201@gmail.com>
2019-05-09 17:48:53 -0400

    added html layout and tested to see if accounts can be registered on fron end

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,46 @@
+import React, {Fragment, useState} from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    password: ""
+  });
+
+  const { email, password } = formData;
+
+  const onChange = event => setFormData({ ...formData, [event.target.name]: event.target.value });
+
+  const onSubmit = async event => {
+    event.preventDefault();
+    console.log("Login sucessful!");
+  }
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+      <form className="form" onSubmit={event => onSubmit(event)}>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" value={email} onChange={event => onChange(event)} required />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={event => onChange(event)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
+  )
+}
+
+export default Login;

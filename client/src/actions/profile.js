737074717b25ffb6829d931d06d8bda3658d1f45
Jason Kim <jasonkim4201@gmail.com>
2019-05-28 12:56:44 -0400

    added additional profile actions and reducers including github repos

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,7 +6,9 @@ import {
   PROFILE_ERROR,
   UPDATE_PROFILE,
   CLEAR_PROFILE,
-  ACCOUNT_DELETED
+  ACCOUNT_DELETED,
+  GET_PROFILES,
+  GET_REPOS
 } from "./types.js";
 
 // get current user profile
@@ -24,6 +26,75 @@ export const getCurrentProfile = () => async dispatch => {
       type: PROFILE_ERROR,
       payload: { msg: err.response.statusText, status: err.response.status}
     });
+  }
+};
+
+// get all profiles
+
+export const getProfiles = () => async dispatch => {
+  
+  dispatch({ type: CLEAR_PROFILE });
+
+  try {
+    const res = await axios.get("api/profile");
+
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    });
+
+  } catch (err) {
+    
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status}
+    });
+
+  }
+};
+
+// get profile by id
+
+export const getProfileById = userId => async dispatch => {
+  
+
+  try {
+    const res = await axios.get(`api/profile/user/${userId}`);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+  } catch (err) {
+    
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status}
+    });
+
+  }
+};
+
+// get github repos
+
+export const getGithubRepos = username => async dispatch => {
+  
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`);
+
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
+
+  } catch (err) {
+    
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status}
+    });
+
   }
 };
 
@@ -198,3 +269,4 @@ export const deleteAccount = () => async dispatch => {
   }
  
 }
+
